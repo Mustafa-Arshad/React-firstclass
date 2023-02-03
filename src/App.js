64ec71a8e56e35781baf2a4a86f2800fde709e9f
Mustafa-Arshad <hafiz28mustafa@gmail.com>
2023-02-03 15:29:04 +0500

    work done

--- src/App.js
+++ src/App.js
@@ -2,21 +2,138 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let mobiles = [
+    {
+      name: "a30",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 2000,
+      brand: "samsung"
+    },
+    {
+      name: "note10",
+      ram: "6gb",
+      rom: "128gb",
+      camera: "50px",
+      price: 4000,
+      brand: "samsung"
+    },
+    {
+      name: "s10",
+      ram: "3gb",
+      rom: "128gb",
+      camera: "10px",
+      price: 5000,
+      brand: "samsung"
+    },
+    {
+      name: "iphone4",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "iphone"
+    },
+    {
+      name: "iphone4s",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 20000,
+      brand: "iphone"
+    },
+    {
+      name: "iphone5",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 30000,
+      brand: "iphone"
+    },
+    {
+      name: "iphone6",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 30000,
+      brand: "iphone"
+    },
+    {
+      name: "redminote10",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "xiaomi"
+    },
+    {
+      name: "redminote11",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "xiaomi"
+    },
+    {
+      name: "redmi10",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "xiaomi"
+    },
+    {
+      name: "redmi10pro",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "xiaomi"
+    },
+    {
+      name: "a3s",
+      ram: "4gb",
+      rom: "64gb",
+      camera: "50px",
+      price: 10000,
+      brand: "oppo"
+    },
+    {
+      name: "a4s",
+      ram: "6gb",
+      rom: "128gb",
+      camera: "50px",
+      price: 10000,
+      brand: "oppo"
+    },
+    {
+      name: "a5s",
+      ram: "2gb",
+      rom: "32gb",
+      camera: "50px",
+      price: 10000,
+      brand: "oppo"
+    }
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          mobiles.map((x, i) => {
+            return (
+              <div key={i} className="card">
+                <h3>{x.brand}</h3>
+                <p>{x.name}</p>
+                <p>Rs {x.price}/-</p>
+              </div>
+            )
+          })
+        }
+
+
+
+
       </header>
     </div>
   );
